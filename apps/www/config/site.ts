a4ec4657373caf870004c80359ff3662882a895a
Zach Kelling <z@zeekay.io>
2024-01-08 01:28:41 +0100

    feat: new components (#2144)

--- apps/www/config/site.ts
+++ apps/www/config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   url: "https://ui.luxdefi.com",
   ogImage: "https://ui.luxdefi.com/og.jpg",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
   links: {
     twitter: "https://twitter.com/luxdefi",
     github: "https://github.com/luxdefi/ui",
