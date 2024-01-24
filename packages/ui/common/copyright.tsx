fef5f00295d0de15d8dd054d631d909b507545ed
artemis prime <artemisprimedev@gmail.com>
2024-01-24 06:44:29 -0800

    fixed copyright years bug

--- packages/ui/common/copyright.tsx
+++ packages/ui/common/copyright.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Copyright: React.FC = () => {
 
   const year = new Date().getFullYear()
-  const yearString = (year > 2023) ? `${year} - 2023` : '2023'
+  const yearString = (year > 2023) ? `2023 - ${year}` : '2023'
 
   return (
     <>{`Copyright © ${yearString} Lux Partners Ltd. `} <br className='sm:hidden'/> All rights reserved.</> 
