29f221a92c5ba7fbfd227eeb59f9c64ce8ea8be1
artemis prime <artemisprimedev@gmail.com>
2024-01-27 16:35:33 -0800

    fixed pages router font bug

--- pkgs/luxdefi-ui/next/pages-router-font-vars.tsx
+++ pkgs/luxdefi-ui/next/pages-router-font-vars.tsx
@@ -5,7 +5,7 @@ import type NextFontDesc from './next-font-desc'
 const PagesRouterFontVars: React.FC = () => {
 
   const fontVars = nextFonts.map((fd: NextFontDesc) => (
-    `${fd.cssVar}: ${fd.nextFont.style.fontFamily}`
+    `${fd.cssVar}: ${fd.nextFont.style.fontFamily};`
   )).join('\n')
 
   return <style jsx global>{`
