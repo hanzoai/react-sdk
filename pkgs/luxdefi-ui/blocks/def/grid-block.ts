f905d45ca69c095973b9ac39bf8a464ec2f96753
artem ash <artemisprimedev@gmail.com>
2024-02-09 15:20:05 -0800

    GridBlock style-table-borders support

--- pkgs/luxdefi-ui/blocks/def/grid-block.ts
+++ pkgs/luxdefi-ui/blocks/def/grid-block.ts
@@ -3,6 +3,7 @@ import type GridDef from "./grid-def"
 
 interface GridBlock {
   blockType: 'grid'
+  specifiers?: string
   grid: GridDef
   cells?: Block[] // if no cells, contents will be children
 }
