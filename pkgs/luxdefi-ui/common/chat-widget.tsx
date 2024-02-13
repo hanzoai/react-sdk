4bbb3706e0f3384c9472a3f7ddd9352db36b41d0
erikrakuscek <erik.rakuscek@gmail.com>
2024-02-13 12:07:15 +0100

    chat widget update

--- pkgs/luxdefi-ui/common/chat-widget.tsx
+++ pkgs/luxdefi-ui/common/chat-widget.tsx
@@ -1,35 +1,58 @@
 'use client'
 import React from 'react'
 
-import { MessageCircleMore, X } from 'lucide-react'
+import { X } from 'lucide-react'
 
+import LuxLogo from './icons/lux-logo'
 import { Button, Card } from '../primitives'
 
 const ChatWidget: React.FC<{
   title: string,
   chatbotUrl: string,
+  subtitle?: string,
   question?: string,
+  /* 
+    Currently supports these icons from remix icons (https://remixicon.com/):
+      GlobalLineIcon,
+      ShieldFlashLineIcon,
+      BankCardLineIcon,
+      GroupLineIcon,
+      QuestionnaireLineIcon
+  */
+  suggestedQuestions?: { heading: string, message: string, icon?: string }[]
 }> = ({
   title,
   chatbotUrl,
-  question
+  subtitle,
+  question,
+  suggestedQuestions
 }) => {
 
   const [showChatbot, setShowChatbot] = React.useState<boolean>(false)
 
   const onClick = () => { setShowChatbot(!showChatbot) }
 
-  const iframeSrc = chatbotUrl + (question ? `?question=${question}` : '')
+  const searchParams = new URLSearchParams()
+  if (question) {
+    searchParams.append('question', question)
+  }
+  if (suggestedQuestions) {
+    searchParams.append('sQuestions', suggestedQuestions.map(({ message }) => message).join(','))
+    searchParams.append('sHeadings', suggestedQuestions.map(({ heading }) => heading).join(','))
+    searchParams.append('sIcons', suggestedQuestions.map(({ icon }) => icon).join(','))
+  }
+
+  const iframeSrc = `${chatbotUrl}?${searchParams.toString()}`
 
   return (<>
     <div className={
-      'fixed bottom-0 sm:bottom-20 right-0 w-full h-full ' + 
-      'sm:max-w-[400px] sm:max-h-[550px] sm:px-4 z-[1001] ' +  
+      'fixed bottom-0 sm:bottom-20 right-0 w-full h-full ' +
+      'sm:max-w-[400px] sm:max-h-[550px] sm:px-4 z-[1001] ' +
       (showChatbot ? 'flex' : 'hidden')
     }>
       <Card className='flex flex-col h-full w-full'>
         <div className='flex px-4 py-2 h-12 bg-level-0 items-center justify-between'>
-          <h3 className='font-semibold font-heading'>{title}</h3>
+          <h3 className='font-semibold font-heading'>{title} <span className='opacity-60'>{subtitle}</span></h3>
           <Button onClick={onClick} variant='link' size='icon' className='w-fit sm:hidden'>
             <X />
           </Button>
@@ -44,7 +67,7 @@ const ChatWidget: React.FC<{
       onClick={onClick}
       className='fixed bottom-4 right-0 h-12 w-12 mx-4 rounded-full'
     >
-      {showChatbot ? <X /> : <MessageCircleMore />}
+      {showChatbot ? <X /> : <LuxLogo width={24} height={24} className='mt-2' />}
     </Button>
   </>)
 }
