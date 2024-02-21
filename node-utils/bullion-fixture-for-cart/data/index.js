7b9d8ebbc0aefbef846a311ee03eaf2972bc5b76
artem ash <artemisprimedev@gmail.com>
2024-02-20 22:28:18 -0800

    created bun powered data fixture script

--- node-utils/bullion-fixture-for-cart/data/index.js
+++ node-utils/bullion-fixture-for-cart/data/index.js
@@ -1,93 +1,98 @@
-import AU_B from './AU-B'
-import AG_B from './AG-B'
+import AU_B from './AU-B.js'
+import AG_B from './AG-B.js'
 
 export default {
   t: 'LUX',
   label: 'Lux Bullion',
-  sub: [{
-    t: 'AU',
-    label: 'Lux Gold',
-    img: 'assets/img/cart/bar-gold-1072x1072.png',
-    labelLong: 'Lux Gold Bullion',
-    sub: [
-      {
-        t: 'B',
-        label: 'Minted Bar',
-        sub: AU_B
-      },
-      {
-        t: 'C',
-        label: 'Coin',
-        sub: [
-          {
-            t: '1-OZ',
-            price: 1938
-          }
-        ]
-      },
-      {
-        t: 'MB',
-        label: 'Minted Multibar',
-        sub: [
-          {
-            t: '100x1-G',
-            price: 6221.39
-          }
-        ]
-      },
-      {
-        t: 'GD',
-        label: 'Good Delivery Bar',
-        sub: [
-          {
-            t: '400-OZ',
-            price: 800000
-          }
-        ]
-      }
-    ]
-  }], 
-  sub: [{
-    t: 'AG',
-    img: 'assets/img/cart/bar-silver-1072x1072.png',
-    label: 'Lux Silver',
-    labelLong: 'Lux Silver Bullion',
-    sub: [
-      {
-        t: 'B',
-        label: 'Minted Bar',
-        sub: AG_B
-      }, 
-      {
-        t: 'C',
-        label: 'Coin',
-        sub: [
-          {
-            t: '1-OZ',
-            price: 23.04
-          }
-        ]
-      },
-      {
-        t: 'MB',
-        label: 'Minted Multibar',
-        sub: [
-          {
-            t: '100x1-G',
-            price: 74.15
-          }
-        ]
-      },
-      {
-        t: 'GD',
-        label: 'Good Delivery Bar',
-        sub: [
-          {
-            t: '1000-OZ',
-            price: 22000
-          }
-        ]
-      }
-    ]
-  }] 
+  titleToken: '',
+  sub: [
+    {
+      t: 'AU',
+      titleToken: 'Lux Gold Bullion',
+      label: 'Lux Gold Bullion',
+      img: 'assets/img/cart/bar-gold-1072x1072.png',
+      desc: 'gold desc',
+      sub: [
+        {
+          t: 'B',
+          label: 'Minted Bar',
+          sub: AU_B
+        },
+        {
+          t: 'C',
+          label: 'Coin',
+          sub: [
+            {
+              t: '1-OZ',
+              price: 1938
+            }
+          ]
+        },
+        {
+          t: 'MB',
+          label: 'Minted Multibar',
+          sub: [
+            {
+              t: '100x1-G',
+              price: 6221.39
+            }
+          ]
+        },
+        {
+          t: 'GD',
+          label: 'Good Delivery Bar',
+          sub: [
+            {
+              t: '400-OZ',
+              price: 800000
+            }
+          ]
+        }
+      ]
+    },
+    {
+      t: 'AG',
+      img: 'assets/img/cart/bar-silver-1072x1072.png',
+      desc: 'silver desc',
+      titleToken: 'Lux Silver Bullion',
+      label: 'Lux Silver Bullion',
+      sub: [
+        {
+          t: 'B',
+          label: 'Minted Bar',
+          sub: AG_B
+        }, 
+        {
+          t: 'C',
+          label: 'Coin',
+          sub: [
+            {
+              t: '1-OZ',
+              price: 23.04
+            }
+          ]
+        },
+        {
+          t: 'MB',
+          label: 'Minted Multibar',
+          sub: [
+            {
+              t: '100x1-G',
+              price: 74.15
+            }
+          ]
+        },
+        {
+          t: 'GD',
+          label: 'Good Delivery Bar',
+          sub: [
+            {
+              t: '1000-OZ',
+              price: 22000
+            }
+          ]
+        }
+      ]
+    }
+  ], 
 }
--- node-utils/bullion-fixture-for-cart/index.ts
+++ node-utils/bullion-fixture-for-cart/index.ts
@@ -1,7 +1,7 @@
 import { v4 as unique} from 'uuid'
 import type { Product, Category } from '@hanzo/cart/types'
 
-import d from './data/index'
+import d from './data/index.js'
 
 const TS = '-' // token separator
 
@@ -14,6 +14,7 @@ interface ItemData {
 
 interface CategoryData {
   t: string
+  titleToken?: string
   label: string
   level: number
   img?: string
@@ -21,8 +22,7 @@ interface CategoryData {
   sub: CategoryData[] | ItemData[]
 }
 
-
-const cats = new Map<string, Category>
+//const cats = new Map<string, Category>
 const products: Product[] = []
 
 const massStringFromItemToken = (t: string): string => {
@@ -31,55 +31,53 @@ const massStringFromItemToken = (t: string): string => {
 }
 
 const parseCategoryData = (
-  cd: CategoryData, 
+  parent: CategoryData, 
   titleTokens: string[] = [],
-  skuReduced: string = '', 
+  skuTokens: string[] = [], 
 ): Product[] => {
 
-  
-  if (cd.sub.length > 1 && 'price' in cd.sub[0]) {
-    (cd.sub as ItemData[]).forEach((itemData) => {
+  const tToken = parent.titleToken ?? parent.label
+    // otherwise extra comma if titleToken is '' at this level
+  if (tToken?.length > 0) {
+    titleTokens.push(tToken)   
+  }
+  skuTokens.push(parent.t)
+
+  if (parent.sub.length > 0 && 'price' in parent.sub[0]) {
+
+    (parent.sub as ItemData[]).forEach((item) => {
 
       // Lux Bullion, Gold, 1oz Minted Bar
       const bullionForm = titleTokens.pop()
       const previousTitle = titleTokens.join(', ') // so we support n levels
 
+      skuTokens.push(item.t)
+      const sku = skuTokens.join('-')
+
       const p = {
         id: unique(),
-        sku: `${skuReduced}-${itemData.t}`,
-        title: `${previousTitle}, ${massStringFromItemToken(itemData.t)} ${bullionForm}`,
-        desc: itemData.desc ? itemData.desc : cd.desc,
-        price: itemData.price,
-        img: itemData.img ?? cd.img   
+        sku,
+        title: `${previousTitle}, ${massStringFromItemToken(item.t)} ${bullionForm}`,
+        desc: item.desc ? item.desc : parent.desc,
+        price: item.price,
+        img: item.img ?? parent.img   
       } satisfies Product
       products.push(p)
     })
   }
-  else if (cd.sub.length > 1 && 'sub' in cd.sub[0]) {
-    (cd.sub as CategoryData[]).forEach(({
-      t,
-      label,
-      img,
-      desc,
-      sub  
-    }) => {
-
-      titleTokens.push(label)
-
-      const cat = {
-        t,
-        label,
-        level: cd.level + 1,
-        img: img ?? cd.img, 
-        desc,
-        sub,
-      } satisfies CategoryData
-
-      const sku_ = `${skuReduced}-${t}`
-      parseCategoryData(
-        cat,
-        titleTokens,
-        sku_
+  else if (parent.sub.length > 0 && 'sub' in parent.sub[0]) {
+    (parent.sub as CategoryData[]).forEach(({t, label, img, desc, sub}) => {
+      parseCategoryData({
+          t,
+          label,
+          level: parent.level + 1,
+          img: img ?? parent.img, 
+          desc: desc ?? parent.desc,
+          sub,
+        } satisfies CategoryData,
+          // each sub category must have it's own branch
+        [...titleTokens], 
+        [...skuTokens]
       )
     })
   }
