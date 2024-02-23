1a197bc95d2db157506db48fd5025d1d5017a383
erikrakuscek <erik.rakuscek@gmail.com>
2024-02-23 17:27:39 +0100

    turned checkout to screenful

--- apps/market/src/app/checkout/page.tsx
+++ apps/market/src/app/checkout/page.tsx
@@ -7,7 +7,8 @@ import { Button, Input, Main } from '@hanzo/ui/primitives'
 
 import siteDef from '../../siteDef'
 import { useCommerce } from '@hanzo/cart/service'
-import { ImageBlockComponent, type ImageBlock } from '@hanzo/ui/blocks'
+import { type ImageBlock, type EnhHeadingBlock, type ElementBlock, type ScreenfulBlock } from '@hanzo/ui/blocks'
+import { ScreenfulBlockComponent as Screenful } from '@hanzo/ui/blocks'
 
 const CheckoutPage = () => {
   const c = useCommerce()
@@ -16,16 +17,26 @@ const CheckoutPage = () => {
 
   return (<>
     <Main className='md:flex-row gap-2 md:gap-4 flex justify-center'>
-      <div className='flex flex-col gap-2'>
-        <p>Deposit ${c.cartTotalValue} in Lux Coin (LUX on Lux Mainnet only) to:</p>
-        <strong>0xAdf62DADB9E527fb06509842a4266919Fd85d214</strong>
-        <p>Confirm your transaction hash:</p>
-        <div className='flex gap-2'>
-          <Input placeholder='0x...' />
-          <Button onClick={() => console.log('Payment confirmed')}>Confirm</Button>
-        </div>
-      </div>
-      <ImageBlockComponent block={{blockType: 'image', src: '/assets/img/payment-qr.png', dim: {h: 300, w: 300}, alt: 'payment QR code'} as ImageBlock}/>
+      <Screenful 
+        block={{blockType: 'screenful', contentColumns: [[
+          {blockType: 'enh-heading',
+            specifiers: 'preheading-heading-font',
+            preheading: { text: 'Complete your order', level: 3, mb: 6 },
+            heading: { text: `Deposit $${c.cartTotalValue} in Lux Coin (LUX on Lux Mainnet only) to:`, level: 6, mb: 0 },
+            byline: { text: 'Confirm your transaction hash:', level: 6 },
+          } as EnhHeadingBlock,
+          {blockType: 'element', element: <div className='flex gap-2 w-full'>
+            <Input placeholder='0x...' />
+            <Button onClick={() => console.log('Payment confirmed')}>Confirm</Button>
+          </div>} as ElementBlock,
+          ],
+          [{blockType: 'image',
+            src: '/assets/img/payment-qr.png',
+            dim: {h: 300, w: 300},
+            alt: 'payment QR code'
+          } as ImageBlock
+        ]]} as ScreenfulBlock}
+      />
     </Main>
     <Footer siteDef={siteDef} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
   </>)
