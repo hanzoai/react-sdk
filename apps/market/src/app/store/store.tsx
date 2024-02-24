2bd0e214353abcbcef7c2c7ed732d0061e7b7c38
artem ash <artemisprimedev@gmail.com>
2024-02-24 03:11:17 -0800

    layout: sm TODO: cart in drawer

--- apps/market/src/app/store/store.tsx
+++ apps/market/src/app/store/store.tsx
@@ -20,10 +20,15 @@ const Store: React.FC<{
 }) => {
   const c = useCommerce() 
   const cartColumnClx = 'min-w-[300px] md:min-w-[320px] lg:min-w-[320px] lg:max-w-[360px] xl:min-w-[360px]'
+
+  const widgetClx = 'sticky z-30 bg-background top-[44px] md:top-[80px] mb-6 ' +
+    'flex flex-row gap-x-6 items-start pl-3 lg:pr-8 md:pr-14 py-3 ' + 
+    'sm:w-full sm:left-0 sm:right-0 sm:h-auto sm:px-8' 
+
   return (
     <div className='flex flex-col justify-start items-start'>
-      <CategoryFacetsWidget 
-        className='sticky z-30 bg-background top-[80px] mb-6 flex flex-row gap-x-6 items-start pl-3 lg:pr-8 md:pr-14 py-3' 
+      <CategoryFacetsWidget
+        className={widgetClx} 
         facetClassNames={[
           'grid grid-cols-2 gap-x-4 border-r pr-6 ', 
           'grid grid-cols-4 gap-x-4 '
@@ -31,10 +36,10 @@ const Store: React.FC<{
         facets={siteDef.ext.store.facets}
       />
     <div className={cn('flex flex-row justify-between gap-6 items-stretch', className)}>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 grow'>
+      <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 grow'>
         {c.specifiedItems.map((item) => (<ProductCard item={item} key={item.product.sku} className='rounded-lg'/>))}
       </div> 
-      <div className={'shrink ' + cartColumnClx}>
+      <div className={'shrink hidden md:block ' + cartColumnClx}>
         <div className={'fixed z-50 md:top-[94px] lg:top-auto ' + cartColumnClx}>
           <Cart className=''/>
         </div>
