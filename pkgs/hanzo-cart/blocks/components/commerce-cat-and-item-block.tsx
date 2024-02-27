90568c5218be310c4093f3a2cd804b8874a8e4b3
artem ash <artemisprimedev@gmail.com>
2024-02-27 12:29:21 -0800

    int

---
+++ pkgs/hanzo-cart/blocks/components/commerce-cat-and-item-block.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import type  CommerceCatAndItemBlock from '../def/commerce-cat-and-item-block'
+
+/*
+
+interface CommerceCatAndItemBlock extends Block {
+  blockType: 'commerce-cat-and-item'
+  parentPath: string        // SKU path of parent of Category ... eg, in Market, LXB-AU or LXB-AG.  Next level is Cat.
+  defaultCatPath?: string   // SKU path of default Category... eg, in Market, LXB-AU-B (Minted Bar) 
+}
+
+*/
+
