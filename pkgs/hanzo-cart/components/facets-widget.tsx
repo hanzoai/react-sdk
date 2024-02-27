c4fbb105122ef809dba254e852d45c2e57ad3c4e
artem ash <artemisprimedev@gmail.com>
2024-02-27 12:54:12 -0800

    sep of FacetTogglesWidget and FacetsWidget

--- pkgs/hanzo-cart/components/facets-widget.tsx
+++ pkgs/hanzo-cart/components/facets-widget.tsx
@@ -1,130 +1,28 @@
 'use client'
-import React, { useState, type PropsWithChildren } from 'react'
+import React, { type PropsWithChildren } from 'react'
 
-import { ToggleGroup, ToggleGroupItem,} from "@hanzo/ui/primitives"
 import { cn } from '@hanzo/ui/util'
 
-import type { FacetValue, Facets } from '../../types'
-import FacetImage from './facet-image'
+import type { Facets } from '../types'
 
-export interface FacetMutatorExclusive {
-  val: string | null
-  set(v: string | null): void
-}
-
-export interface FacetMutatorMultiple {
-  val: string[] | null
-  set(v: string[] | null): void
-}
-
-const FacetToggleGroup: React.FC<{
-  facetValues: FacetValue[]
-  mutator: FacetMutatorExclusive | FacetMutatorMultiple
-  exclusive: boolean
-  className?: string
-  isMobile?: boolean
-}> = ({
-  facetValues,
-  mutator,
-  exclusive,
-  className='',
-  isMobile=false
-}) => {
-
-  const [lastRemaining, setLastRemaining] = useState<string | undefined>(undefined)
-
-  const handleValueChangeMultiple = (selected: string[]) => {
-    (mutator as FacetMutatorMultiple).set(selected)
-    if (selected.length === 1) {
-      setLastRemaining(selected[0])
-    }
-    else {
-      setLastRemaining(undefined)   
-    }
-  }
-
-  const handleValueChangeExclusive = (selected: string) => {
-    (mutator as FacetMutatorExclusive).set(selected)
-    if (selected) {
-      setLastRemaining(selected)
-    }
-  }
-
-  if (exclusive) {
-
-    return (
-      <ToggleGroup 
-        type='single' 
-        value={mutator.val as string}
-        variant='default'
-        size={isMobile ? 'sm' : 'default'}
-        onValueChange={handleValueChangeExclusive}
-        className={className}
-      >
-      {facetValues.map((fv, index) => {
-        let rounded = 'none'  
-        if (index === 0) { rounded = 'llg' }
-        else if (index === facetValues.length - 1) { rounded = 'rlg' } 
-        return (
-          <ToggleGroupItem 
-            value={fv.token} 
-            disabled={(lastRemaining && lastRemaining === fv.token || fv.token === mutator.val) as boolean} 
-            aria-label={`Select ${fv.label}`}
-              // @ts-ignore
-            rounded={rounded}
-            key={fv.token}
-          >
-            <span className={cn('flex flex-row justify-center gap-1 h-4 items-center')} >
-              <FacetImage facetValue={fv} />
-              <span className='hidden md:block whitespace-nowrap'>{fv.label}</span>
-            </span>
-          </ToggleGroupItem>
-        )
-      })}
-      </ToggleGroup>  
-    )
-  }
-
-  return (
-    <ToggleGroup 
-      type='multiple'
-      variant='default'
-      value={mutator.val as string[]}
-      rounded='xl' 
-      size={isMobile ? 'sm' : 'default'}
-      onValueChange={handleValueChangeMultiple}
-      className={className}
-    >
-    {facetValues.map((fv) => (
-      <ToggleGroupItem 
-        value={fv.token} 
-        disabled={(lastRemaining && lastRemaining === fv.token) as boolean} 
-        aria-label={`Select ${fv.label}`}
-        key={fv.token}
-      >
-        <span className={cn('flex flex-row justify-center gap-1 h-4 items-center')} >
-          <FacetImage facetValue={fv} />
-          <span className='hidden md:block whitespace-nowrap'>{fv.label}</span>
-        </span>
-      </ToggleGroupItem>
-    ))}
-    </ToggleGroup>  
-  )
-}
+import FacetTogglesWidget, { 
+  type FacetMutatorSingle, 
+  type FacetMutatorMultiple
+} from './facet-toggles-widget'
 
 const FacetsWidget: React.FC<PropsWithChildren & {
   facets: Facets
-  exclusive: boolean
   facetClassNames?: string[]
-  mutators:  FacetMutatorExclusive[] | FacetMutatorMultiple[]
+  mutators:  FacetMutatorSingle[] | FacetMutatorMultiple[]
+  multiple?: boolean
   isMobile?: boolean
   id?: string
   className?: string
 }> = ({
   children,
   facets,
-  exclusive,
   mutators,
+  multiple=false,
   facetClassNames,
   isMobile=false,
   className='',
@@ -134,9 +32,9 @@ const FacetsWidget: React.FC<PropsWithChildren & {
   return (
     <div id={id} className={className} >
     {Object.keys(facets).map((key, i) => (
-      <FacetToggleGroup 
+      <FacetTogglesWidget 
         key={i}
-        exclusive={exclusive}
+        multiple={multiple}
         mutator={mutators[i]} 
         isMobile={isMobile}
         facetValues={facets[parseInt(key)]} 
