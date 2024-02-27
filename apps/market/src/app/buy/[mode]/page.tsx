ea3fc1ae6713143bafe2802ec2cdf646cfe3e3a9
artem ash <artemisprimedev@gmail.com>
2024-02-27 09:06:30 -0800

    fixed facet button gap in 'prod'

--- apps/market/src/app/buy/[mode]/page.tsx
+++ apps/market/src/app/buy/[mode]/page.tsx
@@ -39,6 +39,7 @@ const BuyPage: React.FC<Props> = observer(({
 
   const [catLevel1, setCatLevel1] = useQueryState('lev1') // level 1 facet value (AG / AU)
   const [catLevel2, setCatLevel2] = useQueryState('lev2') // level 2 facet value (B / C / MB / GD)
+  const [selByParam, setSelByParam] = useQueryState('size') // Last two tokens for SKU. eg, '1-OZ'
 
   const [prodLevel1, setProdLevel1] = useQueryState('fac1', parseAsArrayOf(parseAsString).withDefault([]))
   const [prodLevel2, setProdLevel2] = useQueryState('fac2', parseAsArrayOf(parseAsString).withDefault([]))
@@ -84,8 +85,8 @@ const BuyPage: React.FC<Props> = observer(({
   }) => {
 
     const widgetClx = 'flex flex-row justify-between sm:gap-x-6 xs:gap-x-2 items-start'  
-    const facets1Clx = 'grid grid-cols-2 gap-0 ' + (mobile ? '' : 'pr-6 ')
-    const facets2Clx = 'grid grid-cols-4 gap-0 '
+    const facets1Clx = 'grid grid-cols-2 ' + (isCat ? 'gap-0 ' : 'gap-1 ') + (mobile ? '' : 'pr-6 ')
+    const facets2Clx = 'grid grid-cols-4 ' + (isCat ? 'gap-0 ' : 'gap-1 ')
 
     const catMutators = [{val: catLevel1, set: setCatLevel1} , {val: catLevel2, set: setCatLevel2}]
     const prodMutators = [{val: prodLevel1, set: setProdLevel1} , {val: prodLevel2, set: setProdLevel2}]
