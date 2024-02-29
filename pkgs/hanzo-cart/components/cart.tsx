d413ca72ffb4df323d65e286e5a5ab4d5489649a
erikrakuscek <erik.rakuscek@gmail.com>
2024-02-29 15:03:47 +0100

    cart: fix persist order

--- pkgs/hanzo-cart/components/cart.tsx
+++ pkgs/hanzo-cart/components/cart.tsx
@@ -1,26 +1,40 @@
 'use client'
-import React, { type PropsWithChildren } from 'react'
+import React, { type PropsWithChildren, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import { Button } from '@hanzo/ui/primitives'
 import { cn } from '@hanzo/ui/util'
 
-import { useCommerce } from '../service'
+import { persistCart, useCommerce } from '../service'
 import { formatPrice } from '../util'
 
 import CartLineItem from './cart-line-item'
+import { useRouter } from 'next/navigation'
+import { useAuth } from '../../hanzo-auth/service'
 
 
 const Cart: React.FC<PropsWithChildren & {
   className?: string
-  isMobile?: boolean
+  isMobile?: boolean,
+  hideCheckout?: boolean
 }> = observer(({
   children,
   className='',
-  isMobile=false
+  isMobile=false,
+  hideCheckout=false
 }) => {
-
+  const [loadingCheckout, setLoadingCheckout] = useState(false)
   const c = useCommerce()
+  const router = useRouter()
+  const auth = useAuth()
+
+  const checkout = async () => {
+    setLoadingCheckout(true)
+    if (auth.user) {
+      await persistCart(c.cartItems, auth.user.email)
+    }
+    router.push('/checkout')
+  }
   
   return (
     <div className={cn('border p-6 rounded-lg', className)}>
@@ -34,10 +48,14 @@ const Cart: React.FC<PropsWithChildren & {
         <p className='mt-6 text-right border-t pt-1'>TOTAL: {c.cartTotal === 0 ? '0' : formatPrice(c.cartTotal)}</p>
       </>)}
       </div>
-      {c.cartItems.length > 0 && (
-        <div>
-          <Button size='lg' variant='secondary' rounded='lg' className='mt-12 mx-auto'>Checkout</Button>
-        </div> 
+      {c.cartItems.length > 0 && hideCheckout && (
+        <>
+          {!auth.loggedIn ? (
+            <Button size='lg' variant='secondary' rounded='lg' className='mt-12 mx-auto' onClick={() => router.push('/login?redirectUrl=checkout')}>Login</Button>
+          ) : (
+            <Button size='lg' variant='secondary' rounded='lg' className='mt-12 mx-auto' onClick={checkout} disabled={loadingCheckout}>Checkout</Button>
+          )}
+        </>
       )}
     </div>
   )
