c6a673011adbaafa20ae392c42acccc7c5e2dacb
erikrakuscek <erik.rakuscek@gmail.com>
2024-03-11 12:14:47 +0100

    added toaster to checkout

--- packages/commerce/components/checkout/index.tsx
+++ packages/commerce/components/checkout/index.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 import { useForm } from 'react-hook-form'
 
-import { Button, Main, Separator } from '@hanzo/ui/primitives'
+import { Button, Main, Separator, Toaster } from '@hanzo/ui/primitives'
 import { cn } from '@hanzo/ui/util'
 import { useAuth } from '@hanzo/auth/service'
 import { LoginComponent, AuthWidget } from '@hanzo/auth/components'
@@ -86,7 +86,8 @@ const Checkout: React.FC<{toggleCheckout: () => void}> = observer(({toggleChecko
   const steps = [step1, step2, step3, step4]
 
   return (
-    <div className="fixed top-0 left-0 !max-w-none w-full h-full min-h-screen bg-background z-[51]">    
+    <div className="fixed top-0 left-0 !max-w-none w-full h-full min-h-screen bg-background z-[51]">  
+      <Toaster/>  
       <Main className='flex flex-col gap-1 h-full'>
         <div className='flex justify-between'>
           <Button
