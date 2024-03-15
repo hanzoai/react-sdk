8c991c0d9b0037fdd3157696a7d963c59263ef88
erikrakuscek <erik.rakuscek@gmail.com>
2024-03-15 12:35:39 +0100

    added secure payments info

--- packages/commerce/components/checkout/payment/payment-methods/index.tsx
+++ packages/commerce/components/checkout/payment/payment-methods/index.tsx
@@ -1,15 +1,22 @@
+import { LockKeyhole } from 'lucide-react'
 import Amex from './amex'
 import Discover from './discover'
 import Mastercard from './mastercard'
 import Visa from './visa'
+import DinersClub from './diners-club'
+import Jcb from './jcb'
 
 const PaymentMethods: React.FC = () => {
   return (
-    <div className='flex gap-1'>
+    <div className='flex gap-1 items-center'>
+      <LockKeyhole className='w-4 h-4'/>
+      <span className='hidden sm:flex text-sm'>Secure payments with</span>
       <Amex className='w-9 h-5'/>
       <Discover className='w-9 h-5'/>
       <Mastercard className='w-9 h-5'/>
       <Visa className='w-9 h-5'/>
+      <DinersClub className='w-9 h-5'/>
+      <Jcb className='w-9 h-5'/>
     </div>
   )
 }
