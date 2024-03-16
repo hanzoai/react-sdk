4c57bc547da7ce88a90883bf8fe3552e7ee11a1c
Erik Rakušček <erik.rakuscek@gmail.com>
2024-03-16 04:35:22 +0100

    Commerce: Added support for card payments in checkout (#59)
    * added pay with card option to checkout
    * added server action to process payment
    * moved square payment processing to utils
    * added user verification for card payments
    * added google pay and apple pay, restyled payment step on checkout
    * added secure payments info
    * auth@2.0.1 and cmmc@2.1.0; added missing dep
    ---------
    Co-authored-by: artem ash <artemisprimedev@gmail.com>

--- packages/commerce/types/index.ts
+++ packages/commerce/types/index.ts
@@ -2,9 +2,7 @@
 export type { default as CommerceService } from './commerce-service'
 export type { default as Product } from './product'
 export type { default as Category } from './category'
+export type { default as TransactionStatus } from './checkout'
 export * from './line-item'
 export * from './facet'
 export * from './string-mutator'
-
-
-
