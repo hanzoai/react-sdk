4ae5433fa9d5890f99de16f842b95dbc207a80f7
erikrakuscek <erik.rakuscek@gmail.com>
2024-04-15 15:08:31 +0200

    Commerce: updated promos

--- packages/commerce/util/promo-codes.ts
+++ packages/commerce/util/promo-codes.ts
@@ -2,6 +2,13 @@
 
 import type { Promo } from '../types'
 
+const referallCodeDiscountedSkus = [
+  'LXM-CR-B-ABT', 'LXM-CR-B-GM', 'LXM-CR-F-CC', 'LXM-CR-F-IC', 'LXM-CR-E-24G', 'LXM-CR-E-SS', 'LXM-CR-S-RT', // credit cards
+  'LXM-VL-GN', 'LXM-VL-VL', 'LXM-VL-MI', 'LXM-VL-NA', // validators
+  'LXM-PS-PS',  // pass
+  'LXM-AG-B-1_OZ', // silver 
+]
+
 const PROMO_CODES: Promo[] = [
   {
     type: 'percent',
@@ -11,7 +18,7 @@ const PROMO_CODES: Promo[] = [
   {
     type: 'percent',
     code: 'NASSER',
-    value: 20
+    value: 11.11
   },
   {
     type: 'percent',
@@ -33,6 +40,66 @@ const PROMO_CODES: Promo[] = [
     code: 'GENESIS',
     value: 99,
     skus: ['LXM-PS-PS']
+  },
+  {
+    type: 'percent',
+    code: 'ANTJE',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'ASHLEY',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'CALE',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'DARA',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'KENJI',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'MAJOR',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'CYRUS',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'STOBIE',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'SKYLER',
+    value: 33,
+    skus: referallCodeDiscountedSkus
+  },
+  {
+    type: 'percent',
+    code: 'ZEEKAY',
+    value: 33,
+    skus: referallCodeDiscountedSkus
   }
 ]
 
